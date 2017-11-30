d77c36a1081e19acde5b17976a6a97675fa72602
Ramon Wijnands <rayman747@hotmail.com>
2017-11-30 23:23:34 +0100

    Improve the admin page a bit

--- backend/admin.py
+++ backend/admin.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Playlist, Song, PlaylistEntry
 
-# Register your models here.
+
+class PlaylistEntryInline(admin.TabularInline):
+    model = PlaylistEntry
+    # extra = 3
+
+
+class PlaylistAdmin(admin.ModelAdmin):
+    inlines = [PlaylistEntryInline]
+    list_display = ('name', 'created_date', 'modified_date')
+
+
+admin.site.register(Song)
+admin.site.register(Playlist, PlaylistAdmin)
--- backend/models.py
+++ backend/models.py
@@ -7,11 +7,14 @@ from django.db import models
 
 class Playlist(models.Model):
     name = models.CharField(max_length=100)
-    cover_url = models.URLField()
+    cover_url = models.URLField(blank=True)
     # author
 
     created_date = models.DateTimeField('created at', auto_now_add=True)
     modified_date = models.DateTimeField('modified at', auto_now=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Song(models.Model):
@@ -23,7 +26,7 @@ class Song(models.Model):
 
 class PlaylistEntry(models.Model):
     playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
-    song = models.ForeignKey(Song, null=True, on_delete=models.SET_NULL)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
     # author
 
     created_date = models.DateTimeField(auto_now_add=True)
