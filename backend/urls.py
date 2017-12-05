fb00df765a915ca33b3f11a47dc9df929d5a8533
Ramon Wijnands <rayman747@hotmail.com>
2017-12-05 23:43:48 +0100

    Add Staff permissions

--- backend/urls.py
+++ backend/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls import url
+from django.urls import include
 from rest_framework.routers import DefaultRouter
 
 from . import views
@@ -7,3 +9,6 @@ router.register(r'playlists', views.PlaylistViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = router.urls
+urlpatterns += [
+    url(r'^api-auth/', include('rest_framework.urls')),
+]
--- backend/views.py
+++ backend/views.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
+from .permissions import IsStaffOrReadOnly
 from .models import Playlist
 from .serializers import PlaylistSerializer, UserSerializer
 
 
-class PlaylistViewSet(viewsets.ReadOnlyModelViewSet):
+class PlaylistViewSet(viewsets.ModelViewSet):
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsStaffOrReadOnly]
