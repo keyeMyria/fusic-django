3825712d43ebac2545bb9fef7f135ec6b86edf70
Ramon Wijnands <rayman747@hotmail.com>
2017-12-05 23:11:59 +0100

    Fix the urls include

--- fusic/urls.py
+++ fusic/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('api/', backend.urls),
+    path('api/', include('backend.urls')),
     path('admin/', admin.site.urls),
 ]
