17c063d638ea29b586bfee6dc8d578692fc43d86
Ramon Wijnands <rayman747@hotmail.com>
2017-12-07 00:01:10 +0100

    Add a ManyToManyField to Playlist to make queries easier

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
 
-class Playlist(models.Model):
+class Song(models.Model):
     name = models.CharField(max_length=100)
-    cover_url = models.URLField(blank=True)
-    # author
 
     created_date = models.DateTimeField('created at', auto_now_add=True)
     modified_date = models.DateTimeField('modified at', auto_now=True)
@@ -13,11 +11,18 @@ class Playlist(models.Model):
         return self.name
 
 
-class Song(models.Model):
+class Playlist(models.Model):
     name = models.CharField(max_length=100)
+    cover_url = models.URLField(blank=True)
+    # author
 
     created_date = models.DateTimeField('created at', auto_now_add=True)
     modified_date = models.DateTimeField('modified at', auto_now=True)
+
+    songs = models.ManyToManyField(Song, through='PlaylistEntry')
+
+    def __str__(self):
+        return self.name
 
 
 class PlaylistEntry(models.Model):
