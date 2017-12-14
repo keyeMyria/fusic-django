29fa7b58c822066974bc88c6650eae44dbacc63e
Ramon Wijnands <rayman747@hotmail.com>
2017-12-14 22:30:28 +0100

    Add `redux-immutable-state-invariant`

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './App';
 import reducers from './reducers';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+
 import radioUpdater from './radioUpdater';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-let store = createStore(reducers, devToolsEnhancer());
+const middleware =
+  process.env.NODE_ENV !== 'production'
+    ? [require('redux-immutable-state-invariant').default()]
+    : [];
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 render(
   <Provider store={store}>
