5793c7283fa1eb94d0c862ca8adeacc904e78147
Ramon Wijnands <rayman747@hotmail.com>
2017-12-29 22:44:30 +0100

    Retrieve the inital radio on websocket.connect

--- backend/consumers.py
+++ backend/consumers.py
@@ -4,30 +4,52 @@ from channels.binding.websockets import WebsocketBinding
 from channels.generic.websockets import WebsocketDemultiplexer
 
 from .models import Radio, RadioVote
+from .serializers import RadioVoteSerializer, RadioSerializer
 
 logger = logging.getLogger(__name__)
 
 
 class RadioBinding(WebsocketBinding):
     model = Radio
+    fields = []
     stream = "radios"
-    fields = ('id', 'name', 'cover_url', 'created_date', 'modified_date', 'songs', 'votes')
 
     @classmethod
     def group_names(cls, instance):
         return ["radio-updates"]
 
+    def serialize_data(self, instance):
+        return RadioSerializer(instance).data
+
+    @classmethod
+    def trigger_inbound(cls, message, multiplexer, pk, **kwargs):
+        """
+        Overrides base trigger_inbound to handle connect
+        """
+        if message.channel.name == "websocket.connect":
+            logger.info('connect to radio: %s', pk)
+
+            # send the inital value of the instance as a 'read' action
+            instance = cls.model.objects.get(pk=pk)
+            data = cls().serialize(instance, 'read')
+            multiplexer.send(data)
+
+        super(RadioBinding, cls).trigger_inbound(message, **kwargs)
+
 
 class VoteBinding(WebsocketBinding):
     model = RadioVote
+    fields = []
     stream = "radios"
-    fields = ('id', 'owner', 'song', 'radio')
 
     @classmethod
     def group_names(cls, instance):
         group = 'radios-%d' % instance.radio.pk
         logger.info('update on group: %s', group)
         return [group]
+
+    def serialize_data(self, instance):
+        return RadioVoteSerializer(instance).data
 
 
 class Demultiplexer(WebsocketDemultiplexer):
@@ -36,5 +58,4 @@ class Demultiplexer(WebsocketDemultiplexer):
     }
 
     def connection_groups(self, pk, **kwargs):
-        logger.info(repr(kwargs))
         return ['radios-' + pk]
