6497d9b4ab318afa3b0119f529403636c77e40bb
Ramon Wijnands <rayman747@hotmail.com>
2018-02-12 22:07:11 +0100

    Cleanup the NewPage

--- frontend/src/pages/ComponentsPage.js
+++ frontend/src/pages/ComponentsPage.js
@@ -3,6 +3,8 @@ import React from 'react';
 
 import { withStyles } from 'material-ui/styles';
 
+import Button from 'material-ui/Button';
+
 import BrowseItem from '../components/BrowseItem';
 import Search from '../components/Search';
 import Footer from '../components/Footer';
@@ -18,6 +20,16 @@ const styles = theme => ({
 const ComponentsPage = ({ classes }) => {
   return (
     <div className={classes.root}>
+      <Button>Asdf</Button>
+      <Button color="primary">Asdf</Button>
+      <Button color="secondary">Asdf</Button>
+      <Button raised>Asdf</Button>
+      <Button raised color="primary">
+        Asdf
+      </Button>
+      <Button raised color="secondary">
+        Asdf
+      </Button>
       <Search />
       <Footer />
       <BrowseItem />
--- frontend/src/pages/NewPage.js
+++ frontend/src/pages/NewPage.js
@@ -4,7 +4,6 @@ import React from 'react';
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
-import Button from 'material-ui/Button';
 
 import BrowseItem from '../components/BrowseItem';
 import Footer from '../components/Footer';
@@ -26,19 +25,6 @@ const NewPage = ({ classes, theme }) => {
   return (
     <div className={classes.root}>
       <Grid container spacing={24}>
-        <Grid item xs={12}>
-          <Typography>asdf</Typography>
-          <Button>Asdf</Button>
-          <Button color="primary">Asdf</Button>
-          <Button color="secondary">Asdf</Button>
-          <Button raised>Asdf</Button>
-          <Button raised color="primary">
-            Asdf
-          </Button>
-          <Button raised color="secondary">
-            Asdf
-          </Button>
-        </Grid>
         <Grid item xs={6}>
           <BrowseItem />>
         </Grid>
