27af6fe54d6140beec992340356778fb20956fe1
Ramon Wijnands <rayman747@hotmail.com>
2018-03-22 10:51:56 +0100

    Fix warning for <ul> in <p>

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom';
 
 export default () => (
   <div>
-    <p>
-      This is the index page, browse &amp; stuff.
-      <ul>
-        <li>
-          <Link to="/radios/1/">Radio</Link>
-        </li>
-        <li>
-          <Link to="/new/">New</Link>
-        </li>
-        <li>
-          <Link to="/components/">Component list</Link>
-        </li>
-      </ul>
-    </p>
+    <p>This is the index page, browse &amp; stuff.</p>
+    <ul>
+      <li>
+        <Link to="/radios/1/">Radio</Link>
+      </li>
+      <li>
+        <Link to="/new/">New</Link>
+      </li>
+      <li>
+        <Link to="/components/">Component list</Link>
+      </li>
+    </ul>
   </div>
 );
